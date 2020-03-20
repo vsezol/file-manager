29b1ba5c496f9cd004fec2bbd7c7662272b6973a
vsezol <vsevolod.zolotov02@mail.ru>
2020-03-20 18:22:54 +0700

    'add_open_and_delete_file'

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import shutil
 import os
 from kivy.properties import ObjectProperty
 
+from kivy.lang import Builder
+
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 from kivy.clock import Clock
 
@@ -10,6 +14,12 @@ from kivy.app import App
 import kivy
 kivy.require('1.11.0')
 
+def check_file_type(file):
+    path = os.path.join(os.getcwd(), file)
+    if os.path.isfile(path):
+        return 'file'
+    elif os.path.isdir(path):
+        return 'dir'
 
 class FileBlock(Button):
     def __init__(self, **kwargs):
@@ -19,49 +29,69 @@ class FileBlock(Button):
         self.font_size = 25
         self.height = 50
 
-
-class MainWidget(Widget):
+class MainWidget(Screen):
     files_wrapper = ObjectProperty(None)
     files_list = ObjectProperty(None)
 
-    def check_file_type(self, file):
-        path = os.path.join(os.getcwd(), file)
-        if os.path.isfile(path):
-            return 'file'
-        elif os.path.isdir(path):
-            return 'dir'
-
-    def draw_files(self, files, open_dir_callback, open_file_callback):
+    def draw_files(self, files, select_callback):
         self.files_list.clear_widgets()
         self.files_list.height = 0
 
         for file in files:
-            file_type = self.check_file_type(file)
+            file_type = check_file_type(file)
             fileblock = None
             if file_type == 'file':
-                fileblock = FileBlock(text=file, background_color = (0.5, 0., 1., 1), on_press=open_file_callback)
+                fileblock = FileBlock(text=file, background_color = (0.5, 0., 1., 1), on_release=select_callback)
             elif file_type == 'dir':
-                fileblock = FileBlock(text=file, background_color=(0, 0.5, 0.5, 1), on_press=open_dir_callback)
+                fileblock = FileBlock(text=file, background_color=(0, 0.5, 0.5, 1), on_release=select_callback)
                 
             self.files_list.add_widget(fileblock)
             self.files_list.height += fileblock.height / 2 + 5
 
+class TextWidget(Screen):
+    pass
+
+class WidgetManager(ScreenManager):
+    pass
+
+kv = Builder.load_file('filemanager.kv')
 
 class FileManagerApp(App):
     current_path = os.getcwd()
     dirs = os.listdir()
 
-    def open_dir(self, instance):
-        path = os.path.join(self.current_path, instance.text)
+    def open_dir(self, dirname):
+        path = os.path.join(self.current_path, dirname)
         path = os.path.normpath(path)
         self.current_path = path
         os.chdir(self.current_path)
         self.dirs = os.listdir()
+        self.update_files()
 
+    def delete_file(self):
+        file, ftype = self.selcted_file
+        try:
+            if ftype == 'dir':
+                shutil.rmtree(file)
+            elif ftype == 'file':
+                os.remove(file)
+            self.dirs.remove(file)
+        except:
+            pass
         self.update_files()
         
-    def open_file(self, instance):
-        pass
+    def open_file(self):
+        file, ftype = self.selcted_file
+        try:
+            if ftype == 'dir':
+                self.open_dir(file)
+            elif ftype == 'file':
+                pass
+        except:
+            pass
+
+    def select_file(self, instance):
+        self.selcted_file = [instance.text, check_file_type(instance.text)]
 
     def back_up(self):
         path = os.path.join(self.current_path, '..')
@@ -76,7 +106,7 @@ class FileManagerApp(App):
         pass
 
     def update_files(self):
-        self.mw.draw_files(self.dirs, self.open_dir, self.open_file)
+        self.mw.draw_files(self.dirs, self.select_file)
 
     def build(self):
         self.mw = MainWidget()
